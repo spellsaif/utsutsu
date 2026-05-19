5aecb0079cbad49bb9e3ccebb711bc8e189d827c
Saif Ali <this.saif.email@gmail.com>
2026-05-20 01:39:11 +0530

    feat: export public API and devtools interfaces in entrypoint

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Core exports
+export { createUtsutsu, UtsutsuStore } from "./core/store.js";
+export { createCell, Cell } from "./core/cell.js";
+export { Lens } from "./core/lens.js";
+export { frame } from "./core/frame.js";
+
+// React bridge exports
+export { useValue, useValueDeferred } from "./react/index.js";
+export type { Handle } from "./react/index.js";
+export type { Dep } from "./core/dependencyTracker.js";
+
+// Devtools exports
+export { connectDevTools } from "./devtools/index.js";
+export type { DevToolsOptions } from "./devtools/index.js";
